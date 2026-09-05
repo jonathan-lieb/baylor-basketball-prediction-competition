430b86e36f15bdbc0324cc12a11971e1e7363056
Jonathan Lieb <jonathantlieb@gmail.com>
2026-09-05 14:15:01 -0500

    anon key change

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://azsyznlimagzmechvfvk.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "sb_publishable_KThMOaHFB7IS7zdtExvqwg_Vf2KBjfl";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6c3l6bmxpbWFnem1lY2h2ZnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTM2ODAsImV4cCI6MjEwNDEyOTY4MH0.ubLdsOd2-lKDoj1yKMrBdYayCrzLWJdqdAeRkTSsEyA";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
